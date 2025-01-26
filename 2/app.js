286a3eb6a993d0effeff77e7b570d476d6fb812e
niedjaraujos <163071319+niedjaraujos@users.noreply.github.com>
2025-01-26 16:48:08 -0300

    Add files via upload

--- 2/app.js
+++ 2/app.js
@@ -1,25 +1,50 @@
 
 const botao = document.querySelector('button');
-
-let valores = [];
+const inputs = document.querySelectorAll('input[type="number"]')
 
 function ordenarDecrescente() {
-    let input = document.querySelector('input')
-    valores.push(input.value)
-
-    
-    input.value= ''
-
+    let valores = [];
+    for (let i = 0; i < inputs.length; i++) {
+        valores.push(inputs[i].value);
+        inputs[i].value = '';
+    }
     valores.sort((x, y) => y - x);  // Ordena o array em ordem decrescente
-    console.log( valores)
+    console.log(valores)
     return valores;
 }
 
 botao.addEventListener('click', () => {
     const resultado = ordenarDecrescente();
     document.querySelector('p').innerHTML = `Valores em ordem decrescente: ${resultado}`
-})
+});
+
+
+/*
+// outras formas
+
+const inputs = document.querySelectorAll('input[type="number"]');
+const button = document.querySelector('button');
+
+button.addEventListener('click', () => {
+    const numbers = [];
+    inputs.forEach(input => {
+        const value = input.value;
+
+        if (!isNaN(value)) {
+            numbers.push(value);
+        }
+
+    });
+    numbers.sort((a, b) => b - a);// Ordenar os números em ordem decrescente
+    document.querySelector('p').textContent = `Números em ordem decrescente: ${numbers}`;
+});
 
 
 
-
+document.querySelector('button').addEventListener('click', () => {
+    const inputs = document.querySelectorAll('input[type="number"]');
+    const valores = Array.from(inputs).map(input => input.value);// Cria um array para armazenar os valores
+    const valoresOrdenados = valores.sort((a, b) => b - a); // Ordena os valores em ordem decrescente
+    document.querySelector('p').textContent = `Valores em ordem decrescente: ${valoresOrdenados}`;
+});
+*/
